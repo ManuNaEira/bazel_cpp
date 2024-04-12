1e49030c7a6db1adc8b6c350dfbdb371f0fdd606
Manuel Nogueira <63038781+ManuNaEira@users.noreply.github.com>
2024-04-12 11:42:50 +0000

    Add gdb configuration to debug active file!! 🎉
    Possible to debug the unit tests also, but only
    from the debug menu, not from the Testing.

--- src/example/lib2/lib2.hpp
+++ src/example/lib2/lib2.hpp
@@ -1,10 +1,10 @@
 #ifndef EXAMPLE3_LIB2_LIB1
 #define EXAMPLE3_LIB2_LIB1
+#include <string>
 
 class CLib2 {
  public:
-  static void PrintWorld();
+  static std::string PrintWorld();
 };
-
 
 #endif /* EXAMPLE3_LIB2_LIB1 */
