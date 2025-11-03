bdfec02a178e36c1fd3610e1f42c3bc3f3b1b138
Manuel Nogueira <63038781+ManuNaEira@users.noreply.github.com>
2025-11-03 08:39:20 +0000

    Fix python bindings wheel creation
    Add pyi stub file to provide hints to the IDE about the available
    functionality. Use py_wheel strip_path_prefixes attribute to simplify
    imports (and actually make them work) at the wheel client side.

---
+++ src/example/lib1/lib1_py.pyi
@@ -0,0 +1,8 @@
+"""Python bindings for lib1"""
+
+def print_hello() -> str:
+    """Returns "hello" string
+    
+    This function calls CLib1::PrintHello from the underlying C++ library.
+    """
+    ...
